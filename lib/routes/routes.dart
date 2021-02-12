8ed6440594791f27b84a99c34c76a1a7ae113b0d
angelwayar <angelmanuelwayar@gmail.com>
2021-02-12 16:23:59 -0400

    Login y registro terminado

---
+++ lib/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../pages/chat_page.dart';
+import '../pages/loading_page.dart';
+import '../pages/login_page.dart';
+import '../pages/register_page.dart';
+import '../pages/usuarios_pages.dart';
+
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'usuarios': (_) => UsuarioPage(),
+  'chat': (_) => ChatPage(),
+  'login': (_) => LoginPage(),
+  'register': (_) => RegisterPage(),
+  'loading': (_) => LoadingPage(),
+};
